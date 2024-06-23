63d88eca690e0db375d4769aa8b9583ee15355b5
cnlxc <821382992@qq.com>
2024-06-23 17:00:23 +0900

    Card upadte

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,29 @@
 import { lusitana } from "@/app/ui/fonts";
-import { fetchRevenue,fetchLatestInvoices } from '@/app/lib/data';
+import { fetchRevenue,fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
+import { Card } from "../ui/dashboard/cards";
 
 export default async function Page(){
     const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
+    const {      
+        numberOfCustomers,
+        numberOfInvoices,
+        totalPaidInvoices,
+        totalPendingInvoices
+    } = await fetchCardData();
+    
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4" >
-
+                <Card title="Collected" value={totalPaidInvoices} type="collected"/>
+                <Card title="Total Customers" value={totalPaidInvoices} type="customers"/>
+                <Card title="Invoices" value={totalPaidInvoices} type="invoices"/>
+                <Card title="Pending" value={totalPendingInvoices} type="pending"/>
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <RevenueChart revenue={revenue}/>

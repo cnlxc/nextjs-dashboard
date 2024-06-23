53ba6d2185f7131d519d6f567e1d80ffff43263f
cnlxc <821382992@qq.com>
2024-06-23 16:00:33 +0900

    revenue chart create
    revenue chart create

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,21 @@
-export default function Page(){
-    return <p>Dashboard Page</p>
+import { lusitana } from "@/app/ui/fonts";
+import { fetchRevenue } from '@/app/lib/data';
+import RevenueChart from "../ui/dashboard/revenue-chart";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
+
+export default async function Page(){
+    const revenue = await fetchRevenue();
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4" >
+
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <RevenueChart revenue={revenue}/>
+            </div>
+        </main>
+    )
 }
